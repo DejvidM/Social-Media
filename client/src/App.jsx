403b57ba6a8f5eab130e60c54f1548285edf7817
landi <landixhelo02@gmail.com>
2024-03-28 09:54:37 +0100

    Style
    new style has been added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="container-fluid">
         <nav>
           <ul>
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/login" style={{ color: "white" }}>Login</Link>
             </li>
             <li>
-              <Link to="/signup">Signup</Link>
+              <Link to="/signup" style={{ color: "white" }}>
+                Signup
+              </Link>
             </li>
           </ul>
         </nav>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,37 +1,49 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../index.css";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input 
-            type="email" 
-            id="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-          />
+    <div className="container login-box">
+      <div className="row">
+        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="user-box">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="user-box">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">
+              Submit
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </form>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input 
-            type="password" 
-            id="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     </div>
   );
 };
